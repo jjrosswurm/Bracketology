82a12dab39a8ea89a3d5e06cd9657578048abad6
jjrosswurm <jjrosswurm@gmail.com>
2023-08-16 08:39:10 -0400

    Add project files.

---
+++ Bracketology/mainForm.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class mainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picbox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.picbox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picbox1
+        '
+        Me.picbox1.Location = New System.Drawing.Point(42, 41)
+        Me.picbox1.Name = "picbox1"
+        Me.picbox1.Size = New System.Drawing.Size(200, 200)
+        Me.picbox1.TabIndex = 0
+        Me.picbox1.TabStop = False
+        '
+        'mainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1044, 661)
+        Me.Controls.Add(Me.picbox1)
+        Me.Name = "mainForm"
+        Me.Text = "Backetology"
+        CType(Me.picbox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picbox1 As PictureBox
+End Class
